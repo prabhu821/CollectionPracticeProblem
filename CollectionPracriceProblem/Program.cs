e724651b32471e97a31692030d9d70fa4c074db1
rishiraj007 <rishiraj@mounttalent.com>
2023-06-29 18:48:49 +0530

    [Prabhu]Add: Program to count of remaining chapter using Collection concept

--- CollectionPracriceProblem/Program.cs
+++ CollectionPracriceProblem/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Collection Practice Problem!");
+            int n = 4;
+            int k = 7;
+
+            Dictionary<int, int> chapters = new Dictionary<int, int>();
+
+            chapters.Add(1, 4);
+            chapters.Add(5, 7);
+            chapters.Add(9, 16);
+            chapters.Add(17, 26);
+
+            int chaptersToRead = CountChapters.ChaptersToRead(n, chapters, k);
+            Console.WriteLine("The Number of Chapters to Read: " + chaptersToRead);
         }
     }
 }
